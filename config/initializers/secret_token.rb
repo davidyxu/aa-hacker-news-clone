39e5107996417039bc8d76d241da18e2d7733af7
Student <student@appacademy.io>
2013-04-09 09:13:32 -0700

    Add rail files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNewsClone::Application.config.secret_token = '83d28f4654de7c3864e10a440b9d6dd9aa79df509241d3a833f7bac99389261212262de9a0fcf5d8ec0905d6d52dda5492a889692f97cd74f753e55e2f3c6b6d'
